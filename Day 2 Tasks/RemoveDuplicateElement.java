4dd4d44349adfe14ac5ed482845d6e8ec754423e
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-09 10:42:02 +0530

    Remove the duplicate element from array

---
+++ Day 2 Tasks/RemoveDuplicateElement.java
@@ -0,0 +1,35 @@
+package addon;
+import java.util.Scanner;
+
+public class RemoveDuplicateElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        
+        int newSize = 0;
+        for (int i = 0; i < n; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < newSize; j++) {
+                if (arr[i] == arr[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                arr[newSize++] = arr[i];
+            }
+        }
+
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < newSize; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
